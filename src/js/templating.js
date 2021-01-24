2aa3d7b563e81e836f8298ea46a37dd9db1c0137
Marina Permiakova <marinapermyakova13@gmail.com>
2021-01-24 23:36:23 +0200

    Updates

--- src/js/templating.js
+++
@@ -1,7 +0,0 @@
-import itemsTemplate from '../templates/items.hbs';
-import menu from '../menu.json';
-
-const menuItemsRef = document.querySelector('.js-menu');
-const markup = itemsTemplate(menu);
-
-menuItemsRef.insertAdjacentHTML('beforeend', markup);
